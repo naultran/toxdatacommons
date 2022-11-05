2f81fe76556e1d897789bc2a34e3763bb03db3e1
Nault, Rance <naultran@msu.edu>
2022-11-05 14:49:26 -0400

    dictionary was downgraded to original dev version to fix major issues and compilation code was written

---
+++ custom_configs/schema_compile.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  5 14:43:50 2022
+
+@author: Rance Nault
+"""
+
+#import dictionaryutils
+from dictionaryutils import dump_schemas_from_dir
+import json
+
+#import dictionaryutils
+schema = dump_schemas_from_dir(directory="../gdcdictionary/schemas")
+
+with open('schema.json', 'w') as json_file:
+    json.dump(schema, json_file)
