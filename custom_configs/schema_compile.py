4f265bee215e39a75302799e0665c3d405d75e64
shaunzhao666 <shaun311800@gmail.com>
2023-05-10 16:08:08 -0400

    update model

--- custom_configs/schema_compile.py
+++ custom_configs/schema_compile.py
@@ -12,7 +12,7 @@ import json
 #import dictionaryutils
 schema = dump_schemas_from_dir(directory="/Users/apple/toxdatacommons/gdcdictionary/schemas/tox_model")
 
-with open('schema.json', 'w') as json_file:
+with open('/Users/apple/toxdatacommons/custom_configs/schema.json', 'w') as json_file:
     json.dump(schema, json_file)
     
     
